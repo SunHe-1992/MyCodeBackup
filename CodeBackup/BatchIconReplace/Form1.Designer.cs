a0e29d55644a99ddd03d645d4050a579f241ce0f
Krane Sun <claymore.sun@gmail.com>
2019-01-19 22:18:32 +0800

    add: 批量替换图标

---
+++ CodeBackup/BatchIconReplace/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BatchIconReplace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIconFolder = new System.Windows.Forms.Button();
+            this.newIconFolder = new System.Windows.Forms.TextBox();
+            this.textbox2 = new System.Windows.Forms.TextBox();
+            this.btnHongqueFolder = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIconFolder
+            // 
+            this.btnIconFolder.Location = new System.Drawing.Point(129, 94);
+            this.btnIconFolder.Name = "btnIconFolder";
+            this.btnIconFolder.Size = new System.Drawing.Size(233, 23);
+            this.btnIconFolder.TabIndex = 0;
+            this.btnIconFolder.Text = "确认新图标的文件夹";
+            this.btnIconFolder.UseVisualStyleBackColor = true;
+            this.btnIconFolder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // newIconFolder
+            // 
+            this.newIconFolder.Location = new System.Drawing.Point(56, 42);
+            this.newIconFolder.Name = "newIconFolder";
+            this.newIconFolder.Size = new System.Drawing.Size(416, 20);
+            this.newIconFolder.TabIndex = 1;
+            this.newIconFolder.Text = "C:\\Project\\【11月闪联版】\\【2017年六月开发】\\【珑icon】";
+            // 
+            // textbox2
+            // 
+            this.textbox2.Location = new System.Drawing.Point(56, 184);
+            this.textbox2.Name = "textbox2";
+            this.textbox2.Size = new System.Drawing.Size(416, 20);
+            this.textbox2.TabIndex = 3;
+            this.textbox2.Text = "C:\\Project\\Muffin_Domestic_Advance";
+            // 
+            // btnHongqueFolder
+            // 
+            this.btnHongqueFolder.Location = new System.Drawing.Point(129, 233);
+            this.btnHongqueFolder.Name = "btnHongqueFolder";
+            this.btnHongqueFolder.Size = new System.Drawing.Size(275, 23);
+            this.btnHongqueFolder.TabIndex = 5;
+            this.btnHongqueFolder.Text = "确认红雀工程文件夹";
+            this.btnHongqueFolder.UseVisualStyleBackColor = true;
+            this.btnHongqueFolder.Click += new System.EventHandler(this.btnHongqueFolder_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(129, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "换图标(包括安卓和ios)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 482);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnHongqueFolder);
+            this.Controls.Add(this.textbox2);
+            this.Controls.Add(this.newIconFolder);
+            this.Controls.Add(this.btnIconFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIconFolder;
+        private System.Windows.Forms.TextBox newIconFolder;
+        private System.Windows.Forms.TextBox textbox2;
+        private System.Windows.Forms.Button btnHongqueFolder;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CodeBackup/BatchIconReplace/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+namespace BatchIconReplace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class IconFile
+        {
+            public int width;
+            public int height;
+            public string path;
+
+            public IconFile(string _path)
+            {
+                Image _icon = Image.FromFile(_path);
+                width = _icon.Width;
+                height = _icon.Height;
+                _icon.Dispose();
+                path = _path;
+            }
+        }
+        #region android&ios icons
+
+        string[] androidIcons =
+        {
+        "\\SdkAndroidProject\\SDK\\res\\drawable\\app_icon.png" ,
+        "\\SdkAndroidProject\\SDK\\res\\drawable-hdpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK\\res\\drawable-ldpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK\\res\\drawable-mdpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK\\res\\drawable-xhdpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK\\res\\drawable-xxhdpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK_WithGS\\res\\drawable\\app_icon.png",
+        "\\SdkAndroidProject\\SDK_WithGS\\res\\drawable-hdpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK_WithGS\\res\\drawable-ldpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK_WithGS\\res\\drawable-mdpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK_WithGS\\res\\drawable-xhdpi\\app_icon.png",
+        "\\SdkAndroidProject\\SDK_WithGS\\res\\drawable-xxhdpi\\app_icon.png"
+        };
+        string[] iosIcons =
+        {
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\114.png",
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\120.png",
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\144.png",
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\152.png",
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\180.png",
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\57.png",
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\72.png",
+            "\\SdkXcodePriject\\Images.xcassets\\AppIcon.appiconset\\76.png",
+        };
+
+        #endregion
+
+
+        string[] files;
+        string projectFolder;
+        List<IconFile> newIcons = new List<IconFile>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            files = Directory.GetFiles(newIconFolder.Text, "*.png");
+            newIcons.Clear();
+            foreach (string file in files)
+            {
+                newIcons.Add(new IconFile(file));
+            }
+            MessageBox.Show("成功读取新图标!");
+        }
+
+        private void btnHongqueFolder_Click(object sender, EventArgs e)
+        {
+            projectFolder = textbox2.Text;
+            MessageBox.Show("成功读取工程路径!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //开始批量替换图片
+            foreach (string oldIconPath in androidIcons)
+            {
+                IconFile oldFile = new IconFile(projectFolder + oldIconPath);
+                string newIconPath = FindNewIcon(oldFile.width, oldFile.height);
+                File.Copy(newIconPath, projectFolder + oldIconPath, true);
+            }
+
+            foreach (string oldIconPath in iosIcons)
+            {
+                IconFile oldFile = new IconFile(projectFolder + oldIconPath);
+                string newIconPath = FindNewIcon(oldFile.width, oldFile.height);
+                if (string.IsNullOrEmpty(newIconPath) == false)
+                {
+                    File.Copy(newIconPath, projectFolder + oldIconPath, true);
+                }
+            }
+            MessageBox.Show("批量替换图标完成!");
+        }
+
+        /// <summary>
+        /// 根据原来的icon 长度宽度 找到对应新icon的路径
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        string FindNewIcon(int width, int height)
+        {
+            foreach (IconFile icon in newIcons)
+            {
+                if (icon.height == height && icon.width == width)
+                {
+                    return icon.path;
+                }
+            }
+            return null;
+        }
+    }
+}
+
