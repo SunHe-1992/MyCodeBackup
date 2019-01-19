61ea7cd5214f05e7c4676af26354399fa1d69daf
Krane Sun <claymore.sun@gmail.com>
2019-01-19 21:58:01 +0800

    add: （Levenshtein距离）算法

---
+++ CodeBackup/Levenshtein/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Written by Krane Sun 2014-11-18 17:15:00
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Levenshtein distance = new Levenshtein();
+        //textBox1 : input first string 输入字符串1
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //textBox2 : input second string 输入字符串2
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        //button3 : read textbox 1 and 2 and call the calucate method and display it by messagebox
+        //读取字符串1和字符串2 然后计算编辑距离  再将结果用messagebox输出
+        private void button3_Click(object sender, EventArgs e)
+        {
+            distance.Str1 = textBox1.Text;
+            distance.Str2 = textBox2.Text;
+            int num = distance.GetDistance();
+            System.Windows.Forms.MessageBox.Show
+                (distance.Str1 + " 和 " + distance.Str2 + " 的编辑距离是 " + num);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kraneSunToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CodeBackup/Levenshtein/Levenshtein.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Written by Krane Sun 2014-11-18 17:15:36
+
+namespace WindowsFormsApplication2
+{
+    class Levenshtein
+    {
+        //declear 2 strings
+        private string str1;
+
+        public string Str1
+        {
+            get { return str1; }
+            set { str1 = value; }
+        }
+        private string str2;
+
+        public string Str2
+        {
+            get { return str2; }
+            set { str2 = value; }
+        }
+        
+        public int GetDistance()
+        {
+            //declear the matrix
+            int[,] d = new int[30, 30];
+
+            //fill in str1 and str2
+            for (int i = 0; i < str1.Length; i++)
+                d[0, i + 1] = str1[i];
+            for (int i = 0; i < str2.Length; i++)
+                d[i + 1, 0] = str2[i];
+
+            //calculate  fill the matrix 
+            for (int i = 1; i < str1.Length + 2; i++)
+                for (int j = 1; j < str2.Length + 2; j++)
+                {
+                    int cost = 0;
+
+                    if (d[0, i] == d[j, 0])
+                    { cost = 0; }
+                    else
+                    { cost = 1; }
+                    //find out which is minum
+                    d[i, j] = d[i - 1, j] + 1;
+
+                    if (d[i, j] > d[i, j - 1] + 1)
+                    {
+                        d[i, j] = d[i, j - 1] + 1;
+                    }
+
+                    if (d[i, j] > d[i - 1, j - 1] + cost)
+                    {
+                        d[i, j] = d[i - 1, j - 1] + cost;
+                    }
+                }
+            return d[str1.Length + 1, str2.Length + 1];//get Levenshtein distance
+        }
+
+
+
+    }
+}
